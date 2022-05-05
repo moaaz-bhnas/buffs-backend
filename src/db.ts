c14824dfba0206f3893d283c48eeac20832ac0c7
moaaz-bhnas <moaaz_bs@yahoo.com>
2022-05-05 13:01:35 +0200

    removed colors

--- src/db.ts
+++ src/db.ts
@@ -7,7 +7,7 @@ const MONGO_URI = process.env.MONGO_URI!;
 
 const connectDB = async () => {
   const conn = await mongoose.connect(MONGO_URI);
-  console.log(`mongodb connected: ${conn.connection.host}`.cyan.underline.bold);
+  console.log(`mongodb connected: ${conn.connection.host}`);
 };
 
 export default connectDB;
