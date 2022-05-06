4e0e836340aedce5f25c70a052725b74bf0cebed
moaaz-bhnas <moaaz_bs@yahoo.com>
2022-05-06 12:02:07 +0200

    Heroku

--- src/playground.ts
+++
@@ -1,9 +0,0 @@
-import crypto from "crypto";
-
-const buffer = crypto.randomBytes(5);
-
-console.log("buffer: ", buffer);
-
-const string = buffer.toString("hex");
-
-console.log("string: ", string);
