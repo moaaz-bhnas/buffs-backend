a3af277606f812c88c6eac03337fde681414620e
Moaaz Bhnas <moaaz_bs@yahoo.com>
2023-08-21 00:26:02 +0300

    WIP: created follow endpoint

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,3 +1,4 @@
+import HttpStatusCode from "@/interfaces/http-status-codes/HttpStatusCode";
 import UserModel from "@/schemas/UserSchema";
 import ErrorResponse from "@/utils/errorResponse";
 import { NextFunction, Request, Response } from "express";
@@ -11,29 +12,88 @@ class UsersController {
    * @access    Public
    */
   async getUsers(req: Request, res: Response, next: NextFunction) {
-    return res.status(200).json(res.advancedResults);
+    return res.status(HttpStatusCode.OK).json(res.advancedResults);
   }
 
   /**
    * @desc      Gets a single user
-   * @route     GET /api/v1/users/:id
+   * @route     GET /api/v1/users/:userId
    * @access    Public
    */
-  async getUser(req: Request, res: Response, next: NextFunction) {
-    const { id } = req.params;
-
+  async getUser(
+    req: Request<{ userId: string }>,
+    res: Response,
+    next: NextFunction
+  ) {
     try {
-      const user = await UserModel.findById(id);
+      const user = await UserModel.findById(req.params.userId);
 
       if (!user) {
         const error = new ErrorResponse({
-          message: `User not found with id: ${id}`,
-          statusCode: 404,
+          message: `User not found with id: ${req.params.userId}`,
+          statusCode: HttpStatusCode.NOT_FOUND,
         });
         return next(error);
       }
 
-      res.status(200).json({ success: true, data: user });
+      res.status(HttpStatusCode.OK).json({ success: true, data: user });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  /**
+   * @desc      Follows a user
+   * @route     PUT /api/v1/users/:userId/follow
+   * @access    Private
+   */
+  async followUser(
+    req: Request<{ userId: string }>,
+    res: Response,
+    next: NextFunction
+  ) {
+    if (!req.user) {
+      return next(
+        new ErrorResponse({
+          message: `Authenticated user is not passed with the req object. Check the auth middleware`,
+          statusCode: HttpStatusCode.INTERNAL_SERVER_ERROR,
+        })
+      );
+    }
+
+    try {
+      // 1. Check whether user exists
+      const userToFollow = await UserModel.findById(req.params.userId);
+      if (!userToFollow) {
+        const error = new ErrorResponse({
+          message: `User not found with id: ${req.params.userId}`,
+          statusCode: HttpStatusCode.NOT_FOUND,
+        });
+        return next(error);
+      }
+
+      // 2. Check whether the user who fired the request is alreasdy a follower
+      const isFollowing = userToFollow.followers.includes(req.user._id);
+      if (isFollowing) {
+        const error = new ErrorResponse({
+          message: `User: ${req.user.username} is already following user: ${userToFollow.username}`,
+          statusCode: HttpStatusCode.BAD_REQUEST,
+        });
+        return next(error);
+      }
+
+      // 3. update both users
+      await UserModel.findByIdAndUpdate(userToFollow._id, {
+        $addToSet: { followers: req.user._id },
+      });
+      const followingUser = await UserModel.findByIdAndUpdate(
+        req.user._id,
+        { $push: { following: userToFollow._id } },
+        { returnDocument: "after" }
+      );
+      res
+        .status(HttpStatusCode.OK)
+        .json({ success: true, data: followingUser });
     } catch (error) {
       next(error);
     }
